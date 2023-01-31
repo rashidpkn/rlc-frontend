f30ae4981e895c0029cb296a0f5da32b8bfe5305
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-01-31 17:02:05 +0530

    Somthing is added

---
+++ src/pages/form/index.jsx
@@ -0,0 +1,203 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import BackendIP from '../../BackendIP'
+
+function DataImport() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [ads, setAds] = useState(
+        {
+        adsTitle: '',
+        phone: {
+          code: '+91',
+          number: ''
+        },
+        intro: '',
+        location: '',
+        nationality: '',
+        language: [],
+        eye: '',
+        hair: '',
+        measurement: {
+          bust: 60,
+          waist: 60,
+          hip: 60
+        },
+        socialMedia: {
+          video: '',
+          website: '',
+          instagram: '',
+          twitter: '',
+          telegram: '',
+          facebook: '',
+          tiktok: ''
+        },
+        height: 160,
+        weight: 55,
+        age: 22,
+        currencyType: 'AED',
+        outCall: {
+          oneHourOut: 40,
+          twoHourOut: 80,
+          nightOut: 200
+        },
+        inCall: {
+          oneHourIn: 40,
+          twoHourIn: 80,
+          nightIn: 200
+        },
+        service: [],
+        profilePhoto: '',
+        gallery: []
+      }
+      )
+
+      const publishAds = ()=>{
+        axios.post(`${BackendIP}/ads/create`, { ads, username, email }).then(res => {
+            if (res.data.status) {
+                window.alert("Ads is created")
+                
+            }
+        })
+      }
+    return (
+        <form onSubmit={e=>{
+            e.preventDefault()
+            publishAds()
+        }} className='flex flex-col justify-center items-center p-[5%] gap-5 border'>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="username">Username</label>
+                <input value={username} onChange={e=>setUsername(e.target.value)} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Email">Email</label>
+                <input value={email} onChange={e=>setEmail(e.target.value)} required className='border-b outline-none' type="email" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="adsTitle">AdsTitle</label>
+                <input value={ads.adsTitle} onChange={e=>setAds({...ads,adsTitle:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Phone NO">Phone NO</label>
+                <div className="w-full space-x-[5%]">
+                    <input value={ads.phone.code}   onChange={e=>setAds({...ads,phone:{...ads.phone,code:e.target.value}})}   required className='w-[20%] border-b outline-none' type="text" />
+                    <input value={ads.phone.number} onChange={e=>setAds({...ads,phone:{...ads.phone,number:e.target.value}})} required className='w-[75%] border-b outline-none' type="number" />
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Introduction">Introduction</label>
+                <textarea value={ads.intro} onChange={e=>setAds({...ads,intro:e.target.value})} required cols={5} className='border-b outline-none min-h-[100px]' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Location">Location</label>
+                <input value={ads.location} onChange={e=>setAds({...ads,location:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Nationality">Nationality</label>
+                <input value={ads.nationality} onChange={e=>setAds({...ads,nationality:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Language">Language</label>
+                <input value={ads.language} onChange={e=>setAds({...ads,language:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Eye">Eye</label>
+                <input value={ads.eye} onChange={e=>setAds({...ads,eye:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Hair">Hair</label>
+                <input value={ads.hair} onChange={e=>setAds({...ads,hair:e.target.value})} required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Hair">Measurements</label>
+                <div className="space-x-[12.5%]">
+                    <input value={ads.measurement.bust}  onChange={e=>setAds({...ads,measurement:{...ads.measurement,bust:e.target.value}})} required className='border-b outline-none w-[25%]' type="number" />
+                    <input value={ads.measurement.hip}   onChange={e=>setAds({...ads,measurement:{...ads.measurement,hip:e.target.value}})} required className='border-b outline-none w-[25%]' type="number" />
+                    <input value={ads.measurement.waist} onChange={e=>setAds({...ads,measurement:{...ads.measurement,waist:e.target.value}})} required className='border-b outline-none w-[25%]' type="number" />
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Hair">Social Media</label>
+                <div className="flex justify-between items-center flex-wrap gap-y-5">
+                    <input value={ads.socialMedia.video}      onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,video:e.target.value}})}     placeholder={"video"}     className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.website}    onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,website:e.target.value}})}   placeholder={"website"}   className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.instagram}  onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,instagram:e.target.value}})} placeholder={"instagram"} className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.twitter}    onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,twitter:e.target.value}})}   placeholder={"twitter"}   className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.telegram}   onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,telegram:e.target.value}})}  placeholder={"telegram"}  className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.facebook}   onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,facebook:e.target.value}})}  placeholder={"facebook"}  className='border-b outline-none w-[48%]' type="text" />
+                    <input value={ads.socialMedia.tiktok}     onChange={e=>setAds({...ads,socialMedia:{...ads.socialMedia,tiktok:e.target.value}})}    placeholder={"tiktok"}    className='border-b outline-none w-[48%]' type="text" />
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Height">Height</label>
+                <input value={ads.height} onChange={e=>setAds({...ads,height:e.target.value})} required className='border-b outline-none' type="number" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Weight">Weight</label>
+                <input value={ads.weight} onChange={e=>setAds({...ads,weight:e.target.value})} required className='border-b outline-none' type="number" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Age">Age</label>
+                <input value={ads.age}    onChange={e=>setAds({...ads,age:e.target.value})}   required className='border-b outline-none' type="number" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Currency">Currency Type</label>
+                <input value={ads.currencyType} onChange={e=>setAds({...ads,currencyType:e.target.value})} required  className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="OutCall">OutCall</label>
+                <div className="flex justify-between items-center flex-wrap gap-y-5">
+                    <input value={ads.outCall.oneHourOut} onChange={e=>setAds({...ads,outCall:{...ads.outCall ,oneHourOut : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                    <input value={ads.outCall.twoHourOut} onChange={e=>setAds({...ads,outCall:{...ads.outCall ,twoHourOut : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                    <input value={ads.outCall.nightOut}   onChange={e=>setAds({...ads,outCall:{...ads.outCall ,nightOut   : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="InCall">InCall</label>
+                <div className="flex justify-between items-center flex-wrap gap-y-5">
+                    <input value={ads.inCall.oneHourIn} onChange={e=>setAds({...ads,inCall:{...ads.inCall, oneHourIn : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                    <input value={ads.inCall.twoHourIn} onChange={e=>setAds({...ads,inCall:{...ads.inCall, twoHourIn : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                    <input value={ads.inCall.nightIn}   onChange={e=>setAds({...ads,inCall:{...ads.inCall, nightIn   : e.target.value}})} className='border-b outline-none w-[48%]' type="number" />
+                </div>
+            </div>
+
+            {/* <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Service">Service</label>
+                <input className='border-b outline-none' type="text" />
+            </div> */}
+            
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="ProfilePhoto">ProfilePhoto</label>
+                <input value={ads.profilePhoto} onChange={e=>setAds({...ads,profilePhoto:e.target.value})}  required className='border-b outline-none' type="text" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full md:w-96">
+                <label htmlFor="Gallery">Gallery</label>
+                <input value={ads.gallery} onChange={e=>setAds({...ads,gallery:e.target.value})}  className='border-b outline-none' type="text" />
+            </div>
+
+            <button className='h-12 bg-rose-800 px-5 rounded-md text-white' type='submit'>Publish Ads</button>
+
+        </form>
+    )
+}
+
+export default DataImport
