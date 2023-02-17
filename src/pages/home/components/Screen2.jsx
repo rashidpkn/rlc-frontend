199e43123557edb804f7d29dfdbd8d961ea5fbbb
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-02-17 17:31:32 +0530

    Somthing is added

--- src/pages/home/components/Screen2.jsx
+++ src/pages/home/components/Screen2.jsx
@@ -29,10 +29,11 @@ function Screen2() {
 
     return (
         <div className='w-full overflow-hidden bg-[#1E1E1E] p-5 space-y-5 ' >
+
+            {/* desktop screen */}
             <div
-                // style={ { transform: `translate(-${count * 370}px,0px)` }} 
                 style={ { transform: `translate(-${count * 25}vw,0px)` }} 
-                className={`flex items-center text-white  gap-5  duration-500 h-[600px]`}>
+                className={`hidden lg:flex  items-center text-white  gap-5  duration-500 h-[600px]`}>
                 <div className="w-1/4 flex-shrink-0 space-y-5">
                     <div className='text-3xl cursor-pointer '>Lorem ipsum dolor sit amet consectetur. Elit dictumst velit mattis nec sed lacus at.</div>
                     <p className='text-justify'>Lorem ipsum dolor sit amet consectetur. Vulputate arcu ornare urna nisi adipiscing penatibus ultrices duis. Magnis eget mauris imperdiet a scelerisque lacus velit cursus. Ut morbi dui diam velit sed elit.</p>
@@ -43,18 +44,28 @@ function Screen2() {
                 <Profile {...ads.find(e => e.id === 5)} order={orders.order4} />
                 <Profile {...ads.find(e => e.id === 6)} order={orders.order5} />
                 <Profile {...ads.find(e => e.id === 7)} order={orders.order6} />
-                {/* {ads.map(e=><div className="w-[230px] h-[270px] flex-shrink-0 relative">
-                    <img src={`${e.profilePhoto}`} className='h-full object-cover rounded-lg w-full  ' alt="" />
-                    <div className="absolute bottom-2 left-2">
-                        <p className='text-lg'>{e.adsTitle}</p>
-                        <p>{e.location}</p>
-                    </div>
-                </div>)} */}
 
             </div>
+
+            {/* mobile screen */}
+            <div 
+                style={ { transform: `translate(-${count * 50}vw,0px)` }} 
+                className={`flex lg:hidden  items-center text-white  gap-5  duration-500 h-[300px]`}>
+                <div className="w-1/2 flex-shrink-0 space-y-5">
+                    <div className=' text-2xl cursor-pointer text-justify'>Lorem ipsum dolor sit amet consectetur. Elit dictumst velit mattis nec sed lacus at.</div>
+                    <p className='text-justify'>Lorem ipsum dolor sit amet consectetur. Vulputate arcu ornare urna nisi adipiscing penatibus ultrices duis. Magnis eget mauris imperdiet a scelerisque lacus velit cursus. Ut morbi dui diam velit sed elit.</p>
+                </div>
+                <Profile {...ads.find(e => e.id === 2)} order={orders.order1} />
+                <Profile {...ads.find(e => e.id === 3)} order={orders.order2} />
+                <Profile {...ads.find(e => e.id === 4)} order={orders.order3} />
+                <Profile {...ads.find(e => e.id === 5)} order={orders.order4} />
+                <Profile {...ads.find(e => e.id === 6)} order={orders.order5} />
+                <Profile {...ads.find(e => e.id === 7)} order={orders.order6} />
+
+            </div>
+            
             <div className="float-right  flex justify-center items-center gap-5 mr-10 text-white">
                 <button className='rounded-full p-1 border' 
-                // onClick={() => count && setCount(count - 1)}
                 onClick={()=>{
                     count && setCount(count - 1)
 
@@ -72,7 +83,6 @@ function Screen2() {
                     <NavigateBefore fontSize='large' />
                 </button>
                 <button className='rounded-full p-1 border' 
-                // onClick={() => ads.length + 1 !== count && setCount(count + 1)}
                 onClick={()=>{
                     6 !== count &&(count ===0 ? setCount(count + 2) : setCount(count + 1))
                     orders.order6 !== 1 && setOrders({
@@ -96,11 +106,10 @@ export default Screen2
 
 const Profile = ({ id, profilePhoto, adsTitle, location, order }) => {
     return (
-        <div className="w-1/4  flex-shrink-0 relative duration-500 overflow-hidden rounded-lg" 
-        style={order===1 ? {height:'500px'} : {height:'400px'}}
+        <div className= {`w-1/2 lg:w-1/4  flex-shrink-0 relative duration-500 overflow-hidden rounded-lg ${order===1 ? 'h-[300px] lg:h-[500px] ' : 'h-[250px] lg:h-[400px]'}`} 
         >
             <Link to={`/profile/${id}`}>
-                <img src={`${profilePhoto}`} className='h-full object-cover  w-full hover:scale-125 duration-500 hover:object-top' alt="" />
+                <img src={`${BackendIP}${profilePhoto}`} className='h-full object-cover  w-full hover:scale-150 lg:hover:scale-125 duration-500 hover:object-top' alt="" />
                 <div className="absolute bottom-2 left-2">
                     <p className='text-lg'>{adsTitle}</p>
                     <p>{location}</p>
--- src/pages/home/components/Screen4.jsx
+++ src/pages/home/components/Screen4.jsx
@@ -6,7 +6,7 @@ function Screen4() {
     const [ads, setAds] = useState([])
     const fetchData = () => {
         axios.get(`${BackendIP}/ads/get-all-ads`).then(res => {
-            setAds(res.data)
+          setAds(res.data.sort((a, b) => a.id - b.id))
         })
     }
     useEffect(() => {
@@ -15,9 +15,15 @@ function Screen4() {
   return (
     <div className='h-screen w-full sticky top-0' >
         <div className="absolute w-full h-full overflow-auto">
-          {
+          <Screen {...ads.find(e => e.id === 2)} zIndex={1} />
+          <Screen {...ads.find(e => e.id === 3)} zIndex={2} />
+          <Screen {...ads.find(e => e.id === 4)} zIndex={3} />
+          <Screen {...ads.find(e => e.id === 5)} zIndex={4} />
+          <Screen {...ads.find(e => e.id === 6)} zIndex={5} />
+          <Screen {...ads.find(e => e.id === 7)} zIndex={6} />
+          {/* {
             ads.map((e,index)=><Screen intro={e.intro} profilePhoto={e.profilePhoto} zIndex={index+1} gallery={e.gallery} />)
-          }
+          } */}
             
         </div>
     </div>
@@ -27,26 +33,39 @@ function Screen4() {
 export default Screen4
 
 
-const Screen = ({zIndex,profilePhoto,gallery,intro}) =>{
+const Screen = ({zIndex,profilePhoto,gallery,intro,adsTitle,location}) =>{
   return(
-    <div className="h-full w-full sticky top-0 flex py-5 px-[5%] gap-5 bg-[#1e1e1e] text-white" style={{zIndex}}>
+    <div className="h-full w-full sticky top-0 flex flex-col lg:flex-row py-5 px-[5%] gap-5 bg-[#1e1e1e] text-white" style={{zIndex}}>
       
-      <div className="h-full w-1/2 border">
-      <img src={profilePhoto} className='h-full w-full' alt="" />
+      <div className="h-3/5 lg:h-full w-full lg:w-1/2  flex flex-col lg:flex-row items-center lg:items-start">
+        
+        <div className="w-1/5 h-full items-end relative hidden lg:flex">
+          <p className='rotate-[270deg] text-8xl text-red-600 absolute bottom-28 -left-14 h-max w-max'>{adsTitle}</p>
+        </div>
+        <img src={profilePhoto} className='h-[80%] lg:h-full  w-full lg:w-3/5 object-cover object-top' alt="" />
+        <div className="lg:hidden ">
+          <p className=' text-2xl text-red-600 text-center'>{adsTitle}</p>
+          <p className=' text-2xl text-red-600 text-center'>{location}</p>
+        </div>
+
+        <div className="w-1/5 h-full  relative hidden lg:flex">
+            <p className='rotate-[270deg] text-3xl text-red-600 absolute top-28 right-0 h-max w-max '>Red Light Club</p>
+        </div>
       </div>
       
-      <div className="h-full w-[30%] flex flex-col justify-end pb-10">
+      <div className="h-1/3 lg:h-full w-full lg:w-[30%] hidden lg:flex flex-col justify-center pb-10 overflow-hidden">
         <p className='text-justify'>{intro}</p>
       </div>
       
-      <div className="h-full w-[20%] flex flex-col gap-5  py-5">
+      <div className="h-1/5 lg:h-full w-full lg:w-[20%] flex flex-row lg:flex-col gap-5  py-5">
         {
-          gallery.map(e=><div className="w-full h-64 border">
+          gallery?.map(e=><div className="w-full h-64 border">
             <img className='h-full w-full object-cover' src={e} alt="" />
           </div>)
         }
-        
       </div>
+
+
     </div>
   )
 }
